f1c9f12b4550c1b053812e5238155eb371872eaa
mikethegaia <mikethegaia@users.noreply.github.com>
2014-11-25 02:00:44 -0600

    Create BorraCatalogo.java

---
+++ src/java/BorraCatalogo.java
@@ -0,0 +1,64 @@
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class BorraCatalogo extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+        String notify="",url="";
+        String id = request.getParameter("idCat");
+
+        DBConnection aux=new DBConnection(getServletContext().getInitParameter("bdAdm").split("/"),getServletContext().getInitParameter("bdHost"),getServletContext().getInitParameter("bdNom"));
+
+        Connection con=aux.connect();
+
+        if(con!=null){
+
+            try{
+
+			PreparedStatement stat = con.prepareStatement("DELETE FROM cliente_cat WHERE id_catalogo='"+id+"'");
+			stat.executeUpdate();
+			stat.executeUpdate("DELETE FROM catalogo_prod WHERE id_catalogo='"+id+"'");
+			stat.executeUpdate("DELETE FROM catalogo WHERE id='"+id+"'");
+
+			aux.disconnect();
+            stat.close();
+
+			url="/mainadmin.jsp";
+
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+
+        }else{
+            notify="Â¡Ups! Tenemos problemas TÃ©cnicos";
+            url="/mainadmin.jsp";
+        }
+
+        RequestDispatcher disp = getServletContext().getRequestDispatcher(url);
+        if(disp!=null){
+            disp.forward(request, response);
+        }
+
+    }
+
+     protected void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+		 try {
+			 doGet(request, response);
+		 } catch (Exception e){
+			 e.printStackTrace();
+		 }
+	 }
+
+}
